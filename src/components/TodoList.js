d779d2f04a4f767102dde0c97152f2da4294f041
anjali-sim <anjalighetia123@gmail.com>
2023-04-27 16:39:11 +0530

    :wrench: Updated the checked functionality in Todo list component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,34 +2,34 @@ import React, { useState } from "react";
 import "../css/todoList.css";
 
 function TodoList() {
-
     const itemList = ['Buy new sweatshirt', 'Begin promotional phase', 'Read an article', 'Try not to fall asleep', 'Watch "Sherlock"', 'Begin QA for the product', 'Go for a walk']; 
+    const [isChecked, setIsChecked] = useState(itemList.map(() => false));
 
-    const [isChecked, setIsChecked] = useState(false);
     const handleCheck = (index) => {
-        setIsChecked(!isChecked)
+        const newChecked = [...isChecked];
+        newChecked[index] = !isChecked[index];
+        setIsChecked(newChecked);
     }
-    const style = {
-        color: isChecked ? 'lightgrey' : 'black'
-    }
- 
+
     return (
-    <div className="container mt-3">
-        {itemList.map((item, index) => {
-            return (
-<div className="row p-2" key={index}>
-        <div className="col-8 d-flex float-start">
-          <p  style={style}>{item}</p>
+        <div className="container mt-3">
+            {itemList.map((item, index) => {
+                const style = {
+                    color: isChecked[index] ? 'lightgrey' : 'black',
+                };
+                return (
+                    <div className="row p-2" key={index}>
+                        <div className="col-8 d-flex float-start">
+                            <p style={style} onClick={() => handleCheck(index)}>{item}</p>
+                        </div>
+                        <div className="col-4 d-flex justify-content-end hh">
+                            <input type="checkbox" className="checkBox" checked={isChecked[index]} onChange={() => handleCheck(index)} />
+                        </div>
+                    </div>
+                );
+            })}
         </div>
-        <div className="col-4 d-flex justify-content-end" key={index}>
-          <input type="checkbox" className="checkBox" onChange={() => handleCheck(index)}></input>
-        </div>
-      </div>
-            );
-        })}
-      
-    </div>
-  );
+    );
 }
 
 export default TodoList;
