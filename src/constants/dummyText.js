9b59295019251a1fae48f930cbc31edbad44f6ca
anjali-sim <anjalighetia123@gmail.com>
2023-04-28 19:57:12 +0530

    :art: Improved the file structure

---
+++ src/constants/dummyText.js
@@ -0,0 +1,35 @@
+export const dayNames = [
+    "SUNDAY",
+    "MONDAY",
+    "TUESDAY",
+    "WEDNESDAY",
+    "THURSDAY",
+    "FRIDAY",
+    "SATURDAY",
+  ];
+
+export const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  //variable to store the list of items
+export const dummyList = [
+  "Buy new sweatshirt",
+  "Begin promotional phase",
+  "Read an article",
+  "Try not to fall asleep",
+  'Watch "Sherlock"',
+  "Begin QA for the product",
+  "Go for a walk",
+];
